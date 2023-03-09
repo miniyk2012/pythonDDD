8b5d061edcf33e2ffcd7a0373d328f80ca3bb12e
yangkai.04 <yangkai.04@bytedance.com>
2023-03-09 09:33:30 +0800

    [test] 增加ref检测

--- test_allocate.py
+++ test_allocate.py
@@ -12,8 +12,9 @@ def test_prefers_current_stock_batches_to_shipments():
     shipment_batch = Batch("shipment-batch", "RETRO-CLOCK", 100, eta=tomorrow)
     line = OrderLine("oref", "RETRO-CLOCK", 10)
 
-    allocate(line, [in_stock_batch, shipment_batch])
+    ref = allocate(line, [in_stock_batch, shipment_batch])
 
+    assert ref == in_stock_batch.reference
     assert in_stock_batch.available_quantity == 90
     assert shipment_batch.available_quantity == 100
 
@@ -42,6 +43,7 @@ def test_returns_allocated_batch_ref():
 def test_raises_out_of_stock_exception_if_cannot_allocate():
     batch = Batch("batch1", "SMALL-FORK", 10, eta=today)
     allocate(OrderLine("order1", "SMALL-FORK", 10), [batch])
+    assert batch.available_quantity == 0
 
     with pytest.raises(OutOfStock, match="SMALL-FORK"):
         allocate(OrderLine("order2", "SMALL-FORK", 1), [batch])
